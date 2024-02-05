462c2e4a27b529b1ca41a7a7d24fa2b815cbba36
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-05 23:35:25 +0700

    speed.py

--- speed.py
+++ speed.py
@@ -1964,7 +1964,7 @@ def shell():
                 ((results.upload / 1000.0 / 1000.0) / args.units[1],
                  args.units[0]),
                 quiet)
-        printer("\n...................................................................................\n▶︎ POWERED BY *OOKLA*\n▶︎ Script By *BOTCAHX*")
+        printer("\n...................................................................................\n▶︎ POWERED BY *OOKLA*\n▶︎ Script By *Iky𝖔𝖋𝖋𝖎𝖈𝖎𝖆𝖑ཽ*")
     else:
         printer('Skipping upload test', quiet)
 
