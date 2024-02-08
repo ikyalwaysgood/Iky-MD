7ce1672b9ba922c3280b1ed239c8c2599541cf9c
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-08 11:16:21 +0700

    info-sewa.js

--- plugins/info-sewa.js
+++ plugins/info-sewa.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command }) => {
-    let buffer = await fetch(`https://telegra.ph/file/1c53a85e78e81e2aaad9c.jpg`).then(res => res.buffer())
-    conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Jika telah melakukan pembayaran silahkan kirimkan bukti pembayaran ke WhatsApp Owner.*`, m)
+    let buffer = await fetch(`https://cdn.btch.bz/file/06e8d93a5831ce577b93e.jpg`).then(res => res.buffer())
+    conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Jika ingin melakukan pembayaran silahkan ketik .owner untuk pembayaran ke WhatsApp Owner.*`, m)
 }
 
-handler.help = handler.command = ['donasi','donate','sewa','sewabot','belibot']
+handler.help = handler.command = ['sewa','sewabot','belibot']
 handler.tags = ['main']
 module.exports = handler
