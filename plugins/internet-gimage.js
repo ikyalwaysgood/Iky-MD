250539332a1793f780519d2015ee48e93c853043
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-09 02:26:46 +0700

    internet-gimage.js

--- plugins/internet-gimage.js
+++ plugins/internet-gimage.js
@@ -7,9 +7,9 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let link = image
     conn.sendFile(m.chat, link, 'google.jpg', `*G O O G L E*\n*Result:* ${text}\n*Source:* https://google.com`, m)
 }
-handler.help = ['gimage <query>', 'image <query>']
+handler.help = ['gimage <query>', 'image <query>', 'foto <query>']
 handler.tags = ['internet']
-handler.command = /^(gimage|image)$/i
+handler.command = /^(gimage|image|foto)$/i
 
 module.exports = handler
 
