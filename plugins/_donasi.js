05a8923d7dee1187bb20d9e30a534e44845130d5
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-23 21:29:14 +0700

    Update plugins

---
+++ plugins/_donasi.js
@@ -0,0 +1,19 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), `
+*Hi bantu donasi yuk supaya bot aktif selalu👋*
+╔═══════════════════
+║ _*DONASI DI SINI*_ 
+╠═══════════════════
+║╭──❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉──────
+║│➸ *DANA* : 085794908894
+║│➸ *PULSA*: 083164864536
+║│➸ *GOPAY*: 085794908894
+║╰──────────────────
+╰═══════════════════
+
+`.trim(), footer, 'Owner', '.owner', 'Menu', '.menu')
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
