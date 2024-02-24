434917c5b0676cf46e4eaa3cbc12ef6107c08e72
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-24 11:16:38 +0700

    _menu3.js

--- plugins/_menu3.js
+++ plugins/_menu3.js
@@ -1,1 +1,8 @@
-_menu3.js
+let handler = async (m, { conn }) => {
+conn.reply(m.chat, audio, m) 
+}
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = /^(menu)$/i
+
+module.exports = handler
