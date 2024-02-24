707b6c4742e42986e450ddd47f8639b53ca9543f
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-24 21:57:50 +0700

    _menu2.js

--- plugins/_menu2.js
+++ plugins/_menu2.js
@@ -25,15 +25,15 @@ ubah di bawah!!!
  conn.sendMessage(m.chat, {
       video: { url: "https://btch.pages.dev/file/e3cdfeaf72fd2d4dbd3e0.mp4"},
       gifPlayback: true,
-      caption: ' *_"Kemauan adalah kunci sukses. Orang-orang sukses, berusaha keras apa pun yang mereka rasakan dengan menerapkan keinginan mereka untuk mengatasi sikap apatis, keraguan atau ketakutan."_*\n\n┏━━━━━━━━•━━━━━━━━━
-┃⬡ Assalamu'alaikum 
-┣━━━━━━━┅┅┅━━━━━━━•
-┃⬡ Hallo %name 👋
-┃⬡ ─❑ [ *𝕀ℕ𝔽𝕆 USER* ] ❑─
-┃⬡ *_Tersisa ➨_* %limit Limit
-┃⬡ *_Role ➨_* %role
-┃⬡ *_Level ➨_* %level (%exp / %maxexp) [%xp4levelup]
-┃⬡ %totalexp *_XP secara Total_*
+      caption: ' *_"Kemauan adalah kunci sukses. Orang-orang sukses, berusaha keras apa pun yang mereka rasakan dengan menerapkan keinginan mereka untuk mengatasi sikap apatis, keraguan atau ketakutan."_*\n\n┏━━━━━━━━•━━━━━━━━━\n
+┃⬡ Assalamu'alaikum\n 
+┣━━━━━━━┅┅┅━━━━━━━•\n
+┃⬡ Hallo %name 👋\n
+┃⬡ ─❑ [ *𝕀ℕ𝔽𝕆 USER* ] ❑─\n
+┃⬡ *_Tersisa ➨_* %limit Limit\n
+┃⬡ *_Role ➨_* %role\n
+┃⬡ *_Level ➨_* %level (%exp / %maxexp) [%xp4levelup]\n
+┃⬡ %totalexp *_XP secara Total_*\n
 ┗━━━━━━━┅┅┅━━━━━━━•\n\n❖━━━[ *Kιɳα Bσƚ* ]━━━❖\n*.allmenu*\n*.owner*',     
       contextInfo: {
       externalAdReply: {
