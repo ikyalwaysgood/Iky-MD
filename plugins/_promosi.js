424c7c8718453cc858360cdf9ec2e5b33458457f
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-24 13:27:44 +0700

    _promosi.js

--- plugins/_promosi.js
+++ plugins/_promosi.js
@@ -2,14 +2,14 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 Hallo Semua 👋🏻
 
-╭─ *DAFTAR LIST BIKIN BOT* 🛒
+╭═ 〔 𝐋𝐈𝐒𝐓 𝐁𝐄𝐑𝐋𝐀𝐍𝐆𝐆𝐀𝐍𝐀𝐍 〕 ═
 ├────── *PREMIUM*──────
-├ _*Khusus Bot pake nomor sendiri*_
-│• _1 Minggu / Rp 15.000_
-│• _2 Minggu / Rp 30.000_
-│• _1 Bulan / Rp 50.000_
-│• _2 Bulan / Rp 95.000_
-╰────────────────────
+├⬡ *Khusus Bot pake nomor sendiri*_
+├⬡ 𝟏 ᗷᑌᒪᗩᑎ *_/ Rp15.000_*
+├⬡ 𝟑 ᗷᑌᒪᗩᑎ *_/ Rp40.000_*
+├⬡ 𝟓 ᗷᑌᒪᗩᑎ *_/ RP70.000_*
+├⬡ ᑭEᖇᗰᗩᑎEᑎ *_/ Rp100.000_*
+└═════════════════════
 
 *• KEUNTUNGAN !!!*
 
@@ -27,19 +27,12 @@ Hallo Semua 👋🏻
 - Bisa Nemenin Ngobrol 
 - Dll.
 
-╭═══ 〔 𝐋𝐈𝐒𝐓 𝐁𝐄𝐑𝐋𝐀𝐍𝐆𝐆𝐀𝐍𝐀𝐍 〕 ════
-├⬡ 𝟏 ᗷᑌᒪᗩᑎ *_/ Rp10.000_*
-├⬡ 𝟑 ᗷᑌᒪᗩᑎ *_/ Rp30.000_*
-├⬡ 𝟓 ᗷᑌᒪᗩᑎ *_/ RP50.000_*
-├⬡ ᑭEᖇᗰᗩᑎEᑎ *_/ Rp100.000_*
-└═══════════════
-
 ╭═══════════════════════
-║╭──❉ 〔 𝐕𝐈𝐀 𝐌𝐎𝐁𝐈𝐋𝐄 〕 ❉──────
+║╭──❉ 〔 𝐕𝐈𝐀 𝐌𝐎𝐁𝐈𝐋𝐄 〕 ❉───
 ║│➸ *DANA* : 085794908894
 ║│➸ *PULSA*: 083164864536
 ║│➸ *GOPAY*: 085794908894
-║╰─────────────────────
+║╰──────────────────────
 ╰═══════════════════════
 Jangan melakukan pembayaran sebelum chat saya!!!
 
