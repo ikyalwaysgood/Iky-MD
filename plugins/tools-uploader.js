ef24e8e531be6e254f3fafbd87adedfe41945bdf
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-26 04:07:12 +0700

    Update tools-uploader.js

--- plugins/tools-uploader.js
+++ plugins/tools-uploader.js
@@ -1,23 +1,22 @@
-const uploadFile = require('../lib/uploadFile')
-const uploadImage = require('../lib/uploadImage')
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
 
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada media yang ditemukan'
+  if (!mime) throw 'No media found'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let fileSizeLimit = 5 * 1024 * 1024 
-  if (media.length > fileSizeLimit) {
-    throw 'Ukuran media tidak boleh melebihi 5MB'
-  }
   let link = await (isTele ? uploadImage : uploadFile)(media)
-  m.reply(`${link}
-${media.length} Byte(s)
-${isTele ? '(Tidak Ada Tanggal Kedaluwarsa)' : '(Expired 24 hours)'}`)
+  m.reply(`📮 *L I N K :*
+${link}
+📊 *S I Z E :* ${media.length} Byte
+📛 *E x p i r e d :* ${isTele ? 'No Expiry Date' : 'Unknown'}`)
 }
-handler.help = ['tourl <reply image>']
-handler.tags = ['sticker']
-handler.command = /^(upload|tourl)$/i
 
-module.exports = handler
+handler.help = ['upload (reply media)', 'tourl (reply media)']
+handler.tags = ['tools']
+handler.command = /^(tourl|upload)$/i
+handler.limit = false
+handler.register = true
+export default handler
