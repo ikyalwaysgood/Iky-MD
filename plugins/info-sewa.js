f81d6c880f8aca0638f84a37d05151b445d1b7ce
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-28 00:24:19 +0700

    Update info-sewa.js

--- plugins/info-sewa.js
+++ plugins/info-sewa.js
@@ -1,19 +1,10 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), `
-*Hi bantu donasi yuk supaya bot aktif selalu👋*
-╔═══════════════════
-║ _*PEMBAYARAN DI SINI*_ 
-╠═══════════════════
-║╭──❉ 〔 *INFO* 〕 ❉──────
-║│➸ *DANA* : 085794908894
-║│➸ *PULSA*: 083164864536
-║│➸ *GOPAY*: 085794908894
-║╰──────────────────
-╰═══════════════════
 
-`.trim(), footer, 'Owner', '.owner', 'Menu', '.menu')
-handler.help = ['donasi','sewa','sewabot','belibot']
-handler.tags = ['info']
-handler.command = /^dona(te|si)$/i
+let handler = async (m, { conn, command }) => {
+    let buffer = await fetch(`https://cdn.btch.bz/file/06e8d93a5831ce577b93e.jpg`).then(res => res.buffer())
+    conn.sendFile(m.chat, buffer, 'hasil.jpg', `hhhh`, m)
+}
 
+handler.help = handler.command = ['sewa','sewabot','belibot']
+handler.tags = ['main']
 module.exports = handler
