42e3f1716ce604a2dcbfc737d233341910088e01
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-28 00:26:01 +0700

    Update info-sewa.js

--- plugins/info-sewa.js
+++ plugins/info-sewa.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command }) => {
-    let buffer = await fetch(`https://cdn.btch.bz/file/06e8d93a5831ce577b93e.jpg`).then(res => res.buffer())
+    let buffer = await fetch(`https://telegra.ph/file/ec1342450424ae0bf13ba.jpg`).then(res => res.buffer())
     conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Hi bantu donasi yuk supaya bot aktif selalu👋*
 ╔═══════════════════
 ║ _*PEMBAYARAN DI SINI*_ 
