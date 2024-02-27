e6881fdc5b0f735efc9e8685ab64b4be99c1d42a
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-28 02:56:47 +0700

    Update _donasi.js

--- plugins/_donasi.js
+++ plugins/_donasi.js
@@ -1,19 +1,11 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), `
-*Hi bantu donasi yuk supaya bot aktif selalu👋*
-╔═══════════════════
-║ _*DONASI DI SINI*_ 
-╠═══════════════════
-║╭──❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉──────
-║│➸ *DANA* : 085794908894
-║│➸ *PULSA*: 083164864536
-║│➸ *GOPAY*: 085794908894
-║╰──────────────────
-╰═══════════════════
 
-`.trim(), footer, 'Owner', '.owner', 'Menu', '.menu')
-handler.help = ['donasi']
-handler.tags = ['info']
-handler.command = /^dona(te|si)$/i
+let handler = async (m, { conn, command }) => {
+    let buffer = await fetch(`https://telegra.ph/file/ec1342450424ae0bf13ba.jpg`).then(res => res.buffer())
+    conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Hi bantu donasi yuk supaya bot aktif selalu👋*\n
+╔═══════════════════\n║ _*PEMBAYARAN DI SINI*_\n╠═══════════════════\n║╭──❉ 〔 *INFO* 〕 ❉─────\n║│➸ *DANA* : 085794908894\n║│➸ *PULSA*: 083164864536\n║│➸ *GOPAY*: 085794908894\n║╰──────────────────\n╰═══════════════════`, m)
+}
 
+handler.help = handler.command = ['donasi']
+handler.tags = ['main']
 module.exports = handler
