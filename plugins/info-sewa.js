bf0b90c9fedb7bd66f76c761a8ba4de56b8dc2ba
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-28 02:51:49 +0700

    Update info-sewa.js

--- plugins/info-sewa.js
+++ plugins/info-sewa.js
@@ -2,18 +2,10 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command }) => {
     let buffer = await fetch(`https://telegra.ph/file/ec1342450424ae0bf13ba.jpg`).then(res => res.buffer())
-    conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Hi bantu donasi yuk supaya bot aktif selalu👋*
-╔═══════════════════
-║ _*PEMBAYARAN DI SINI*_ 
-╠═══════════════════
-║╭──❉ 〔 *INFO* 〕 ❉──────
-║│➸ *DANA* : 085794908894
-║│➸ *PULSA*: 083164864536
-║│➸ *GOPAY*: 085794908894
-║╰──────────────────
-╰═══════════════════`, m)
+    conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Hi bantu donasi yuk supaya bot aktif selalu👋*\n
+╔═══════════════════\n║ _*PEMBAYARAN DI SINI*_\n╠═══════════════════\n║╭──❉ 〔 *INFO* 〕 ❉─────\n║│➸ *DANA* : 085794908894\n║│➸ *PULSA*: 083164864536\n║│➸ *GOPAY*: 085794908894\n║╰──────────────────\n╰═══════════════════`, m)
 }
 
-handler.help = handler.command = ['donasi','sewa','sewabot','belibot']
+handler.help = handler.command = ['sewa','sewabot','belibot']
 handler.tags = ['main']
 module.exports = handler
