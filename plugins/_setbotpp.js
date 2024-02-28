e7e03de5f61d9c29be7466248fc13d2ffea0c105
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-28 23:33:25 +0700

    Update _setbotpp.js

--- plugins/_setbotpp.js
+++ plugins/_setbotpp.js
@@ -1,22 +1,16 @@
-/*
-   Silahkan Di Pakek
-   Tapi Bantu Rapihin :v
-   Buatan: Miaweers
-*/
-
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn }) => {
     let bot = conn.user.jid // Bot
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (/image/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw `Foto tidak ditemukan`
-     conn.updateProfilePicture (bot, img)
-    conn.reply(m.chat, 'Sukses Mengganti Foto Profile Bot!', m)
-	}
+        let img = await q.download()
+        if (!img) throw `balas gambarnya!`
+        conn.updateProfilePicture(bot, img)
+        m.reply('Berhasil!')
     }
+}
 handler.help = ['setbotpp']
-handler.command = /^set(ppbot|botpp)$/i
+handler.command = /^(setbotpp)$/i
 handler.owner = true
 
 module.exports = handler
