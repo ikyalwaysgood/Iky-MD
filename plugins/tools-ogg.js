28e0676798e425f146629dc356fc1a834f6ab889
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-28 12:36:33 +0700

    Update tools-ogg.js

--- plugins/tools-ogg.js
+++ plugins/tools-ogg.js
@@ -1,27 +1,19 @@
-const { toAudio, toPTT } = require('../lib/converter')
+const { toAudio } = require('../lib/converter')
+const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (/mp3|a(udio)?$/i.test(command)) {
-    if (!/video|audio/.test(mime)) throw `Balas video/audio dengan perintah *${usedPrefix + command}*`
-    let media = await q.download()
-    if (!media) throw 'Media tidak dapat diunduh'
-    let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw 'Gagal melakukan konversi.'
-    conn.sendMessage(m.chat, {document: audio.data, mimetype: 'audio/mpeg', fileName: `audio.mp3`}, { quoted : m })
-  }
-  if (/vn|ptt$/i.test(command)) {
-    if (!/video|audio/.test(mime)) throw `Balas video/audio dengan perintah *${usedPrefix + command}*`
-    let media = await q.download()
-    if (!media) throw 'Media tidak dapat diunduh'
-    let audio = await toPTT(media, 'mp4')
-    if (!audio.data) throw 'Gagal melakukan konversi.'
-    await conn.sendFile(m.chat, audio.data, 'file.mp4', '', m, 1, { mimetype: 'audio/mp4' })
-  }
+  if (!/video|audio/.test(mime)) throw `Balas video atau voice note yang ingin diubah ke mp3 dengan caption *${usedPrefix + command}*`
+  let media = await q.download()
+  let audio = await toAudio(media, 'mp4')
+  conn.sendMessage(m.chat, audio, MessageType.audio, {
+    quoted: m, mimetype: 'audio/mp4'
+  })
 }
-handler.help = ['tomp3', 'tovn']
-handler.tags = ['Pengubah Suara']
-handler.command = /^to(mp3|vn|ptt)$/i
+handler.help = ['tomp3 (reply)']
+handler.tags = ['audio']
+
+handler.command = /^to(mp3|a(udio)?)$/i
 
 module.exports = handler
